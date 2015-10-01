844038dc7b8941d9cd6525a568c3efeba9d482c3
Esa <esa.huovinen@student.hamk.fi>
2015-10-01 12:58:41 +0300

    changes:

--- outputControl.py
+++ outputControl.py
@@ -3,30 +3,94 @@ import os
 import select
 import time
 import sysv_ipc
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
+
+redLED = False
+greenLED = False
+yellowLED = False
+
+def redGreenYellow(pin, zeroOne):
+    GPIO.output(pin,zeroOne)
+
+def switchRed():
+    global redLED
+    if not redLED:
+        redGreenYellow(11, 1)
+        redLED = True
+    else:
+        redGreenYellow(11, 0)
+        redLED = False
+
+def switchGreen():
+    global greenLED
+    if not greenLED:
+        redGreenYellow(15, 1)
+        greenLED = True
+    else:
+        redGreenYellow(15, 0)
+        greenLED = False
+
+def switchYellow():
+    global yellowLED
+    if not yellowLED:
+        redGreenYellow(13, 1)
+        yellowLED = True
+    else:
+        redGreenYellow(13, 0)
+        yellowLED = False
+
+def flash():
+    for i in range(5):
+        switchRed()
+        time.sleep(1)
+        switchRed()
+        time.sleep(1)
+        switchGreen()
+        time.sleep(1)
+        switchGreen()
+        time.sleep(1)
+        switchYellow()
+        time.sleep(1)
+        switchYellow()
+        time.sleep(1)
+        
 
 commandPipeFp="/tmp/commandPipe"
 
 commands={
-        "setRed": lambda: print("setting Red"),
-        "resetRed": lambda: print("resetting Red"),
+	"switchRed": switchRed,
+        "flash": flash,
+        "setRed": lambda: redGreenYellow(11, 1),
+        "resetRed": lambda: redGreenYellow(11, 0),
         "setYellow": lambda: print("setting Yellow"),
         "resetYellow": lambda: print("resetting Yellow"),
         "setGreen": lambda: print("setting Green"),
         "resetGreen": lambda: print("resetting Green")
         }
 
-
 if os.path.exists(commandPipeFp):
     print("pipe already exists!")
     os.unlink(commandPipeFp)
     print("deleting..")
+
+
 try:
     if os.path.exists(commandPipeFp):
         print("failed to delete old pipe! closing")
         exit(1)
     else:
-        os.mkfifo(commandPipeFp, mode=0o660)
-        svSM=sysv_ipc.SharedMemory(242424, sysv_ipc.IPC_CREX, mode=0o660, init_character=b"0")
+        os.mkfifo(commandPipeFp, 0o666)
+        os.system("sudo chmod 0666 "+commandPipeFp)
+        try:
+            svSM=sysv_ipc.SharedMemory(242424, sysv_ipc.IPC_CREAT, mode=0o660, init_character=b"0", size=64)
+        except sysv_ipc.ExistentialError as e:
+            raise e
+            #svSM=sysv_ipc.attach(242424)
         while True:
             inPipe=open(commandPipeFp,"r")
             readReady, writeReady, xList= select.select([inPipe],[],[])
@@ -50,12 +114,5 @@ finally:
     os.unlink(commandPipeFp)
     svSM.remove()
     svSM.detach()
+    GPIO.cleanup()
     print("cleaned up")
-
-
-# In[67]:
-
-svSM=sysv_ipc.SharedMemory(242424, sysv_ipc.IPC_CREX, mode=0o660, init_character=b"0")
-svSM.remove()
-svSM.detach()
-
