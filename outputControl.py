58e39040f35ea9fb85cd434a2a82258f16063fff
Esa <esa.huovinen@student.hamk.fi>
2015-10-05 11:07:41 +0300

    towards output to shared memory

--- outputControl.py
+++ outputControl.py
@@ -1,26 +1,60 @@
+
+# coding: utf-8
+
+# In[ ]:
+
 #!/usr/bin/python3
 import os
 import select
 import time
 import sysv_ipc
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(15,GPIO.OUT)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(11,GPIO.OUT)
+#GPIO.setup(13,GPIO.OUT)
+#GPIO.setup(15,GPIO.OUT)
 
 redLED = False
 greenLED = False
 yellowLED = False
-
+svSM=sysv_ipc.SharedMemory(0x3000,sysv_ipc.IPC_CREAT,0o666,64)
+with os.fdopen(os.open('/tmp/note', os.O_WRONLY | os.O_CREAT, 0o666), 'w') as fl:
+    fl.write(str(svSM.id))
+svSM.write(b"0000000000000000000000000000000000000000000000000000000000000000")
 def redGreenYellow(pin, zeroOne):
-    GPIO.output(pin,zeroOne)
-   
+    #GPIO.output(pin,zeroOne)
+    pass
+
+
+def red(zeroOne):
+    #GPIO.output(11,zeroOne)
+    if zeroOne==1:
+        svSM.write(b"T",0)
+    else:
+        svSM.write(b"F",0)
+
+def yellow(zeroOne):
+    #GPIO.output(13,zeroOne)
+    if zeroOne==1:
+        svSM.write(b"T",1)
+    else:
+        svSM.write(b"F",1)
+
+def green(zeroOne):
+    #GPIO.output(15,zeroOne)
+    if zeroOne==1:
+        svSM.write(b"T",2)
+    else:
+        svSM.write(b"F",2)
+
 def turnOff():
-    GPIO.output(11, 0)
-    GPIO.output(13, 0)
-    GPIO.output(15, 0)
+    #GPIO.output(11, 0)
+    pass
+    #GPIO.output(13, 0)
+    pass
+    #GPIO.output(15, 0)
+    pass
 
 def switchRed():
     global redLED
@@ -39,7 +73,7 @@ def switchGreen():
     else:
         redGreenYellow(15, 0)
         greenLED = False
- 
+
 def switchYellow():
     global yellowLED
     if not yellowLED:
@@ -63,21 +97,28 @@ def flash():
         time.sleep(1)
         switchYellow()
         time.sleep(1)
-        
 
 commandPipeFp="/tmp/commandPipe"
 
 commands={
-	"switchRed": switchRed,
+        "switchRed": switchRed,
         "flash": flash,
         "turnOff": turnOff,
+        "setRed": lambda: red(1),
+        "resetRed": lambda: red(0),
+        "setYellow": lambda: yellow(1),
+        "resetYellow": lambda: yellow(0),
+        "setGreen": lambda: green(1),
+        "resetGreen": lambda: green(0),
+        }
+"""
         "setRed": lambda: redGreenYellow(11, 1),
         "resetRed": lambda: redGreenYellow(11, 0),
         "setYellow": lambda: redGreenYellow(13, 1),
         "resetYellow": lambda: redGreenYellow(13, 0),
         "setGreen": lambda: redGreenYellow(15, 1),
         "resetGreen": lambda: redGreenYellow(15, 0)
-        }
+"""        
 
 if os.path.exists(commandPipeFp):
     print("pipe already exists!")
@@ -91,12 +132,7 @@ try:
         exit(1)
     else:
         os.mkfifo(commandPipeFp, 0o666)
-        os.system("sudo chmod 0666 "+commandPipeFp)
-        try:
-            svSM=sysv_ipc.SharedMemory(242424, sysv_ipc.IPC_CREAT, mode=0o660, init_character=b"0", size=64)
-        except sysv_ipc.ExistentialError as e:
-            raise e
-            #svSM=sysv_ipc.attach(242424)
+        #os.system("sudo chmod 0666 "+commandPipeFp)
         while True:
             inPipe=open(commandPipeFp,"r")
             readReady, writeReady, xList= select.select([inPipe],[],[])
@@ -121,5 +157,7 @@ finally:
     os.unlink(commandPipeFp)
     svSM.remove()
     svSM.detach()
-    GPIO.cleanup()
+    #GPIO.cleanup()
+    pass
     print("cleaned up")
+
